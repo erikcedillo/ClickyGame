682a6da57468e5171b236ad66ab68ea5edbc0839
Erik Cedillo <erikcedillo@Eriks-MacBook-Pro.local>
2019-02-02 11:44:57 -0800

    working clicky Game

---
+++ App/src/App.js
@@ -0,0 +1,104 @@
+import React, { Component } from "react";
+import Card from "./components/Card";
+import Nav from "./components/Nav";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Container from "./container";
+import Row from "./Row";
+import Column from "./Column";
+import Friends from "./Friends.json";
+import "./App.css";
+
+function shuffleFriends(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+class App extends Component {
+  // Set this.state
+  state = {
+    Friends,
+    currentScore: 0,
+    topScore: 0,
+    rightWrong: "",
+    clicked: [],
+  };
+
+  handleClick = id => {
+    if (this.state.clicked.indexOf(id) === -1) {
+      this.handleIncrement();
+      this.setState({ clicked: this.state.clicked.concat(id) });
+    } else {
+      this.handleReset();
+    }
+  };
+
+  handleIncrement = () => {
+    const newScore = this.state.currentScore + 1;
+    this.setState({
+      currentScore: newScore,
+      rightWrong: ""
+    });
+    if (newScore >= this.state.topScore) {
+      this.setState({ topScore: newScore });
+    }
+    else if (newScore === 12) {
+      this.setState({ rightWrong: "You win!" });
+    }
+    this.handleShuffle();
+  };
+
+  handleReset = () => {
+    this.setState({
+      currentScore: 0,
+      topScore: this.state.topScore,
+      rightWrong: "Play Again!",
+      clicked: []
+    });
+    this.handleShuffle();
+  };
+
+  handleShuffle = () => {
+    let shuffledFriends = shuffleFriends(Friends);
+    this.setState({ friends: shuffledFriends });
+  };
+
+  render() {
+    return (
+      <Wrapper>
+        <Nav
+          Title="Dragon Ball Z Clicky Game"
+          score={this.state.currentScore}
+          topScore={this.state.topScore}
+          rightWrong={this.state.rightWrong}
+        />
+
+        <Title>
+          Choose your characters but don't choose them twice.
+
+        </Title>
+        <Container>
+          <Row>
+            {this.state.Friends.map(friend => (
+              <Column size="md-2 sm-6">
+                <Card
+                  key={friend.id}
+                  handleClick={this.handleClick}
+                  handleIncrement={this.handleIncrement}
+                  handleReset={this.handleReset}
+                  handleShuffle={this.handleShuffle}
+                  id={friend.id}
+                  image={friend.image}
+                />
+              </Column>
+            ))};
+          </Row>
+        </Container>
+      </Wrapper>
+    );
+  }
+}
+export default App;
